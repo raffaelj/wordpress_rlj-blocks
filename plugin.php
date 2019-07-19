23e57a09211d08d4b9d04e6c88a2749687ccdcd6
Raffael <github@raffaeljesche.de>
2019-07-19 13:29:35 +0200

    bump version to 0.1.1, cleanup

--- plugin.php
+++ plugin.php
@@ -2,22 +2,16 @@
 /**
  * Plugin Name: rlj-blocks
  * Plugin URI: https://github.com/raffaelj/rlj-blocks
- * Description: A simple wrapper block for Gutenberg with a background image
+ * Description: Custom Gutenberg blocks - videolink and section with background image
  * Author: Raffael Jesche
  * Author URI: https://www.rlj.me
- * Version: 0.1.0
+ * Version: 0.1.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
  * @package CGB
  */
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if (!defined('ABSPATH')) exit;
 
-/**
- * Block Initializer.
- */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
